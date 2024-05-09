1f51ee0db23323e33d26fbb8578e6ab816f2cd64
Neema-Charity <ncharityyy@gmail.com>
2024-05-09 12:05:45 +0300

    assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,11 @@
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
-/* define the NavBar component */
 function NavBar() {
   return (
     <nav>
       <NavLink
         to="/"
-        /* add styling to Navlink */
         className="nav-link"
       >
         Home
